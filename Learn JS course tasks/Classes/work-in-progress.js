715cf917bad5178b5ff7ed11fef1dd741581e9af
futura <0864380@gmail.com>
2023-02-02 17:20:19 +0100

    updated exceptions

--- Learn JS course tasks/Classes/work-in-progress.js
+++ Learn JS course tasks/Classes/work-in-progress.js
@@ -1,43 +1,17 @@
 'use strict'
-let eventMixin = {
-  on(eventName, handler) {
-    if (!this._eventHandlers) this._eventHandlers = {};
-    if (!this._eventHandlers[eventName]) {
-      this._eventHandlers[eventName] = [];
-    }
-
-    this._eventHandlers[eventName].push(handler);
-  },
-
-  off(eventName, handler) {
-    let handlers = this._eventHandlers && this._eventHandlers[eventName];
-    if (!handlers) return;
-    for (let i = 0; i < handlers.length; i++) {
-      if (handlers[i] === handler) {
-        handlers.splice(i--, 1);
-      }
-    }
-  },
-
-  trigger(eventName, ...args) {
-    if (!this._eventHandlers || !this._eventHandlers[eventName]) {
-      return;
-    }
-
-    this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
-  }
-};
-
-class Menu {
-  choose(value) {
-    this.trigger('select', value);
+class FormatError extends SyntaxError {
+  constructor(desc) {
+    super(desc);
+    this.name = this.constructor.name;
   }
 }
 
-Object.assign(Menu.prototype, eventMixin);
+let err = new FormatError('Oshibka formatirovaniya');
 
-let menu = new Menu();
+console.log(err.message);
+console.log(err.name);
+console.log(err.stack);
 
-menu.on('select', value => console.log(`Selected value: ${value}`));
+console.log(err instanceof FormatError);
+console.log(err instanceof SyntaxError);
 
-menu.choose('123');
